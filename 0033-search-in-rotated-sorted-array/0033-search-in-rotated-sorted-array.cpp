19ece082f2725c05bd054853ae31337a5a0ee26f
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-02-04 12:07:32 +0530

    Time: 3 ms (84.12%), Space: 11.2 MB (41.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -12,26 +12,26 @@ public:
             }
             if(nums[left]<=nums[mid])
             {
-                if(target<nums[left]
-                   || target>nums[mid]
+                if(target<nums[left] //4,5,6 --false
+                   || target>nums[mid] 
                   ){
-                    left = mid+1;  //
+                    left = mid+1;  //0,1,2
                 }
                 else
                 {
-                    right = mid-1; //5
+                    right = mid-1; //4,5,6
                 }
             }
             else 
             {
-                if(target>nums[right] 
+                if(target>nums[right]  
                    || target<nums[mid]
                   ){
-                    right = mid-1; //6
+                    right = mid-1; //6,4,5
                 }
                 else 
                 {
-                    left = mid+1; //0
+                    left = mid+1; //0,2,1
                 }
             }
         }
