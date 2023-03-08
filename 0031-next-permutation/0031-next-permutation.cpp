94fd46f66c1acbd5b3be1d5fea5b266d51a491bf
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-03-08 09:55:03 +0530

    Time: 4 ms (64.28%), Space: 12 MB (76.35%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -31,8 +31,13 @@ public:
             swap(nums[x1],nums[x2]);
             sort(nums.begin()+x1+1,nums.end());
         }
+        
     }
 };
 //very imp sum
 // logic:
-// 
+// 123 so next will be the next big number as a whole which is 132
+// fix 1st digit 
+// 41532 go from behind and see if i+1 is less than ith
+// swap those and sort the numbers after ith
+// this gets u 42135 which comes after 41532
