2cc4a140495ee0b25455942e4453ed4e7cd343fa
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-03-19 10:34:50 +0530

    Time: 56 ms (57.53%), Space: 65.5 MB (27.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    //even--> 
+//odd-->
     string longestPalindrome(string s) {
         int start=0, end=1;
         string ans="";
         for(int i=1;i<s.length();i++)
         {
+            //even
             int low=i-1;
             int high=i;
             while(low>=0 && high<s.length() && s[low]==s[high]){
-                if(high-low+1>end){
+                if(high-low+1>end){//find max
                     end=high-low+1;
                     start=low;
                 }
                 low--;
                 high++;
             }
+            //odd
             low=i-1;
             high=i+1;
             while(low>=0 && high<s.length() && s[low]==s[high]){
