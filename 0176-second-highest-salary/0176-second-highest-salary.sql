574764af2dfe60e8722fe11added445b707253ab
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-07-08 22:19:10 +0530

    Time: 476 ms (58.69%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,4 +2,7 @@
 select ifnull((select distinct salary
 from Employee
 order by salary desc
-limit 1 offset 1), null) as SecondHighestSalary
+limit 1 offset 1 ), null) as SecondHighestSalary
+
+# select distinct salary as SecondHighestSalary
+# from Employee order by salary desc limit 1 offset 1
