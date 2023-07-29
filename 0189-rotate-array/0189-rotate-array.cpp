0dc06579cf323b418a8a6978d362fd0bb289b514
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-07-29 22:49:14 +0530

    Time: 19 ms (99.84%), Space: 24.8 MB (86.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void rev(vector<int>& nums,int low, int high)
-    {
-        while(low<high){
-            swap(nums[low],nums[high]);
-            low++;
-            high--;
-        }
-    }
-    void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
-        rev(nums,0,nums.size()-k-1);
-        rev(nums,nums.size()-k,nums.size()-1);
-        rev(nums,0,nums.size()-1);
+//     void rev(vector<int>& nums,int low, int high)
+//     {
+//         while(low<high){
+//             swap(nums[low],nums[high]);
+//             low++;
+//             high--;
+//         }
+//     }
+     void rotate(vector<int>& nums, int k) {
+         k=k%nums.size();
+    reverse(nums.begin(),nums.end());
+     reverse(nums.begin(),nums.begin()+(k));
+     reverse(nums.begin()+k,nums.end());
+//         
+//         rev(nums,0,nums.size()-k-1);
+//         rev(nums,nums.size()-k,nums.size()-1);
+//         rev(nums,0,nums.size()-1);
       
-    }
+  }
+    
 };
