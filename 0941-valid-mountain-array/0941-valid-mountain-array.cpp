283d1da150b9d5c0489aa4440030150205d4ac3e
Sanika Ardekar <80094199+sanikaardekar@users.noreply.github.com>
2023-08-03 15:08:53 +0530

    Time: 27 ms (82.25%), Space: 22.5 MB (23.99%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        // bool ans; 
+        // if(arr.size()<=2) return false;
+        // int max=INT_MAX, ind=-1;
+        // for(int i=0;i<arr.size();i++)
+        // {
+        //     if(arr[i]>max)
+        //     {
+        //         max=arr[i];
+        //         ind=i;
+        //     }
+        // }
+        // if(ind==0 || ind==arr.size()-1) return false;
+        // for(int i=0;i<ind;i++)
+        // {
+        //     if(arr[i]>=arr[i+1]) return false;
+        // }
+        // for(int j=ind+1;j<arr.size();j++)
+        // {
+        //     if(arr[j]<=arr[j+1]) return false;
+        // }
+        // return true;
+        if(arr.size()<3) return false;
+        int max = INT_MIN;
+        int mountain = -1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>max){
+                max=arr[i];
+                mountain =i;
+            }
+        }
+        if(mountain==0 || mountain==arr.size()-1) return false;
+
+            for(int i=0; i<mountain; i++){
+                if(arr[i]>=arr[i+1]){
+                    return false;
+                }
+            }
+            for(int j=mountain+1; j<arr.size(); j++){
+                if(arr[j]>=arr[j-1]){
+                    return false;
+            }
+        }
+        return true;
+    }
+};
